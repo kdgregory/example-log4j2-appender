2787906ed9625c30da02f12809f12829b7ac1951
Keith Gregory <contact@kdgregory.com>
2020-03-26 08:39:54 -0400

    stage-4: turn the Builder into a configuration object

--- src/main/java/com/kdgregory/sandbox/log4j2/MyAppender.java
+++ src/main/java/com/kdgregory/sandbox/log4j2/MyAppender.java
@@ -33,6 +33,15 @@ import org.apache.logging.log4j.core.config.plugins.validation.constraints.Requi
 @Plugin(name = "MyAppender", category = Core.CATEGORY_NAME, elementType = Appender.ELEMENT_TYPE)
 public class MyAppender extends AbstractAppender
 {
+    private interface MyAppenderConfig
+    {
+        String getName();
+        Layout<String> getLayout();
+        Filter getFilter();
+        int getRepeats();
+    }
+
+
     @PluginBuilderFactory
     public static MyAppenderBuilder newBuilder() {
         return new MyAppenderBuilder();
@@ -40,7 +49,7 @@ public class MyAppender extends AbstractAppender
 
 
     public static class MyAppenderBuilder
-    implements org.apache.logging.log4j.core.util.Builder<MyAppender>
+    implements org.apache.logging.log4j.core.util.Builder<MyAppender>, MyAppenderConfig
     {
         @PluginBuilderAttribute("name")
         @Required(message = "MyAppender: no name provided")
@@ -55,10 +64,22 @@ public class MyAppender extends AbstractAppender
         @PluginBuilderAttribute("repeats")
         private int repeats = 1;
 
+        @Override
+        public String getName()
+        {
+            return this.name;
+        }
+
         public MyAppenderBuilder setName(String value)
         {
             this.name = value;
             return this;
+        }
+
+        @Override
+        public Layout<String> getLayout()
+        {
+            return this.layout;
         }
 
         public MyAppenderBuilder setLayout(Layout<String> value)
@@ -67,10 +88,22 @@ public class MyAppender extends AbstractAppender
             return this;
         }
 
+        @Override
+        public Filter getFilter()
+        {
+            return this.filter;
+        }
+
         public MyAppenderBuilder setFilter(Filter value)
         {
             this.filter = value;
             return this;
+        }
+
+        @Override
+        public int getRepeats()
+        {
+            return this.repeats;
         }
 
         public MyAppenderBuilder setRepeats(int value)
@@ -82,7 +115,7 @@ public class MyAppender extends AbstractAppender
         @Override
         public MyAppender build()
         {
-            return new MyAppender(name, filter, layout, repeats);
+            return new MyAppender(this);
         }
     }
 
@@ -90,21 +123,21 @@ public class MyAppender extends AbstractAppender
 //  Appender implementation
 //----------------------------------------------------------------------------
 
-    private int repeats;
+    private MyAppenderConfig config;
 
 
     @SuppressWarnings("deprecation")
-    private MyAppender(String name, Filter filter, Layout<String> layout, int repeats)
+    private MyAppender(MyAppenderConfig config)
     {
-        super(name, filter, layout);
-        this.repeats = repeats;
+        super(config.getName(), config.getFilter(), config.getLayout());
+        this.config = config;
     }
 
 
     @Override
     public void append(LogEvent event)
     {
-        for (int ii = 0 ; ii < repeats ; ii++)
+        for (int ii = 0 ; ii < config.getRepeats() ; ii++)
         {
             System.out.println(getLayout().toSerializable(event));
         }
